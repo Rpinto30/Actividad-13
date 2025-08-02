872f618ec66012cc9b01f75f23a112a6d85abb9b
Rpinto30 <rodrigopinto3007@gmail.com>
2025-08-01 20:11:00 -0600

    Función que permite agregar estudiantes al diccionario

--- actividad_13.py
+++ actividad_13.py
@@ -1,3 +1,21 @@
+students = {}
+
+def add_student():
+    print("-"*20+" AÑADIR ESTUDIANTE "+"-"*20)
+    while True:
+        id = input("▶ Ingresa el codigo del nuevo estudiante: ")
+        if id not in students: break
+        else: print("\nError, codigo ya registrado, porfavor, crea un codigo no existente")
+    name = input("▶ Ingresa el nombre nuevo estudiante: ")
+    career = input("▶ Ingresa el nombre de la carrera del nuevo estudiante: ")
+
+    students[id] = {
+        'name': name,
+        'career': career,
+        'subjects': []
+    }
+
+    print(f"\n  ✔️ ¡El estudiante {name}({id}) ha sido agregado correctamente!")
 
 
 def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA VALIDA
@@ -5,8 +23,7 @@ def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA V
         try:
             value = int(input(message))
             break
-        except ValueError: print('-'*50+'\n'+"✖"*5+"   Lo siento valor no valido, intentelo nuevamente   "+"✖"*5)
-        except Exception as e: print('-'*50+'\n'+"✖"*5+"   Lo siento, ocurrió un error inesperado, intentelo nuevamente   "+f":{e}"+"✖"*5)
+        except ValueError: print('-'*50+'\n'+"❌"*5+"   Lo siento valor no valido, intentelo nuevamente   "+"❌"*5)
     return value
 
 
@@ -16,9 +33,10 @@ while True:
     try:
         op = input("▶ Ingresa una de las opciones: ")
         match op:
-            case '7':
+            case '1': add_student()
+            case '7': #EL PROGRAMA MARCA UN MENSAJE DE DESPEDIDA Y GUARDA LOS DATOS
                 print("\n  ⌂ Hasta pronto!")
                 break
             case _: print("-"*25+"\nEntrada no valida, intente de nuevo\n"+"-"*25)
-    except KeyboardInterrupt: print("\n\n"+ "❌ Error en el programa, usted intentó salir de manera bruzca\n   Redirigiendo al menú principal")
-    except Exception as e:  print("\n\n"+ f"❌ Error en el programa, hubo un fallo inesperado, {e}\n   Redirigiendo al menú principal")
+    except KeyboardInterrupt: print("\n\n"+ "❌ Error en el programa, usted intentó salir de manera bruzca\n   Redirigiendo al menú principal") #ERROR CUANDO SE FUERZA EL CIERRE DEL PROGRAMA
+    except Exception as e:  print("\n\n"+ f"❌ Error en el programa, hubo un fallo inesperado, {e}\n   Redirigiendo al menú principal") #EVITAR CUALQUIER TIPO DE ERROR NO PREVISTO
