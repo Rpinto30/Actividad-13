66663b4cf5e4e91ed06c4d7210a7432f2ec6fd5b
Rpinto30 <rodrigopinto3007@gmail.com>
2025-08-01 21:10:05 -0600

    Función que permite agregar notas a los estudiantes por medio del nombre

--- actividad_13.py
+++ actividad_13.py
@@ -1,22 +1,53 @@
-students = {}
+students = {'EST1507325':
+                {'name': 'Rodrigo', 'career': 'Ingenieria en Informatica y Sistemas',
+                 'subjects': {'calculo': 10}}}
 
 def add_student():
     print("-"*20+" AÑADIR ESTUDIANTE "+"-"*20)
     while True:
-        id = input("▶ Ingresa el codigo del nuevo estudiante: ")
-        if id not in students: break
+        id_add = input("▶ Ingresa el codigo del nuevo estudiante: ")
+        if id_add not in students: break
         else: print("\nError, codigo ya registrado, porfavor, crea un codigo no existente")
     name = input("▶ Ingresa el nombre nuevo estudiante: ")
     career = input("▶ Ingresa el nombre de la carrera del nuevo estudiante: ")
 
-    students[id] = {
+    students[id_add] = {
         'name': name,
         'career': career,
-        'subjects': []
+        'subjects': {}
     }
 
-    print(f"\n  ✔️ ¡El estudiante {name}({id}) ha sido agregado correctamente!")
+    print(f"\n  ✔️ ¡El estudiante {name}({id_add}) ha sido agregado correctamente!")
 
+def add_note():
+    while True:
+        print("-" * 20 + " AÑADIR NOTA A ESTUDIANTE " + "-" * 20)
+        id_select = input("▶ Ingresa el codigo del estudiante: ")
+        if id_select in students: break #SALE DIRECTAMENTE EL BUCLE SOLO SI EL ID ESTÁ REGISTRADO
+        else:
+            print(f"\n ❌ Lo sentimos, no tenemos registrado el codigo {id}")
+            while True:
+                print("  1) Volver a intentar\n  2) Volver al menú principal")
+                op_cancel_notes = input("▶ ¿Qué deseas hacer?: ")
+                match op_cancel_notes:
+                    case '1': break #VOLVER A INTENTAR
+                    case '2':
+                        id_select = None #DEJA EL ID COMO NONE PARA SABER QUE EL USUARIO QUIERE VOLVER AL MENÚ PRINCIAPAL (ALGO FORZADO)
+                        break
+                    case _: print("-"*25+"\nEntrada no valida, intente de nuevo\n"+"-"*25)
+        if id_select is None: break #VOLVER AL MENU PRINCIPAL
+
+    if id_select in students:
+        print(f"Para el estudiante {students[id_select]['name']}: ")
+        name_subject = input("   ▶ Ingresa el nombre del curso: ")
+        while True:
+            note = input_integer("   ▶ Ingresa la nota final del curso: ")
+            if 0<= note <= 100: break
+            else: print("-"*25+"\nEntrada no valida, la nota debe estar en el rango entre 0 y 100\n"+"-"*25)
+
+        students[id_select]['subjects'][name_subject.lower()] = note
+        print(f"\n  ✔️ ¡El curso {name_subject}({note} pts) ha sido añadido al estudiante {students[id_select]['name']}({id_select})!")
+        print(students)
 
 def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA VALIDA
     while True:
@@ -34,9 +65,10 @@ while True:
         op = input("▶ Ingresa una de las opciones: ")
         match op:
             case '1': add_student()
+            case '2': add_note()
             case '7': #EL PROGRAMA MARCA UN MENSAJE DE DESPEDIDA Y GUARDA LOS DATOS
                 print("\n  ⌂ Hasta pronto!")
                 break
             case _: print("-"*25+"\nEntrada no valida, intente de nuevo\n"+"-"*25)
-    except KeyboardInterrupt: print("\n\n"+ "❌ Error en el programa, usted intentó salir de manera bruzca\n   Redirigiendo al menú principal") #ERROR CUANDO SE FUERZA EL CIERRE DEL PROGRAMA
+    #except KeyboardInterrupt: print("\n\n"+ "❌ Error en el programa, usted intentó salir de manera bruzca\n   Redirigiendo al menú principal") #ERROR CUANDO SE FUERZA EL CIERRE DEL PROGRAMA
     except Exception as e:  print("\n\n"+ f"❌ Error en el programa, hubo un fallo inesperado, {e}\n   Redirigiendo al menú principal") #EVITAR CUALQUIER TIPO DE ERROR NO PREVISTO
